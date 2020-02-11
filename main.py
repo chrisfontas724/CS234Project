0cc9e33f8f5cca05e1908221ba41791d4939ff82
Chris Fontas <chrisfontas724@gmail.com>
2020-02-11 16:00:35 -0600

    Add more boilerplate code
    Add skeleton for grid and start working on file parsing

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import numpy as np
 from optparse import OptionParser
 
 
+color_dictionary = {
+  # TODO - Fill this out
+}
+
+
+def loadfile(filename):
+	file = open(filename, "r") 
+	print(file.read())
+
+
 def main():
     print("Hello, world!")
 
-    parser = OptionParser(usage="usage: %prog [options] filename",
+    parser = OptionParser(usage="usage: %prog -l filename",
                           version="%prog 1.0")
 
     parser.add_option("-l", "--level",
@@ -14,9 +24,11 @@ def main():
                       default="easy-1.txt",
                       help="FlowFree level to load",)
     (options, args) = parser.parse_args()
-
-
     print("Loading level ", options.level)
+
+
+    loadfile("levels/" + options.level)
+
 
 if __name__ == "__main__":
 	main()
