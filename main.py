fae21a52baeca7c1ba02df9dca1e24d9d685cf06
Chris Fontas <chrisfontas724@gmail.com>
2020-02-11 15:48:03 -0600

    add options

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import numpy as np
+from optparse import OptionParser
 
+
+def main():
+    print("Hello, world!")
+
+    parser = OptionParser(usage="usage: %prog [options] filename",
+                          version="%prog 1.0")
+
+    parser.add_option("-l", "--level",
+                      action="store", # optional because action defaults to "store"
+                      dest="level",
+                      default="easy-1.txt",
+                      help="FlowFree level to load",)
+    (options, args) = parser.parse_args()
+
+
+    print("Loading level ", options.level)
 
 if __name__ == "__main__":
-    print("Hello, world!")
+	main()
