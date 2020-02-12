7b82a353e6fedfbdcabb16ca9dc5816877ff516a
Chris Fontas <chrisfontas724@gmail.com>
2020-02-11 19:36:49 -0600

    Start filling out grid class

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ import cv2
 from optparse import OptionParser
 from models.grid import Grid
 
-
-color_dictionary = {
-  # TODO - Fill this out
-}
-
-
-def loadfile(filename):
-	file = open(filename, "r") 
-	print(file.read())
-
-
-def main():
-    print("Hello, world!")
-
+# Use the OptionParser library to get command line arguments
+# for us, such as the file we want to load in.
+def get_options():
     parser = OptionParser(usage="usage: %prog -l filename",
                           version="%prog 1.0")
 
@@ -25,18 +14,22 @@ def main():
                       dest="level",
                       default="easy-1.txt",
                       help="FlowFree level to load",)
-    (options, args) = parser.parse_args()
-    print("Loading level ", options.level)
+    return parser.parse_args()
 
+def main():
 
-    loadfile("levels/" + options.level)
+    # Grab the options.
+    options, args = get_options()
 
+    # Instantiate the FLowFree grid.
+    grid = Grid(filename="levels/" + options.level)
 
     # Test rendering a dummy image of a circle.
     img = np.zeros((512,512,3), np.uint8)
     img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
     cv2.imshow('image',img)
     cv2.waitKey(0)
+    cv2.destroyWindow('image')
 
 if __name__ == "__main__":
 	main()
--- models/grid.py
+++ models/grid.py
@@ -1,11 +1,34 @@
 import numpy as np
 from optparse import OptionParser
 
+color_dictionary = {
+  # TODO - Fill this out
+}
+
 # This class represents the board to be used
 # in flow free. It has square dimensions N and
 # contains a number of pairs of colored dots
 # located at unique locations (i,j)
 # TODO: Fill this out more.
 class Grid:
-    def __init__(self):
+    def __init__(self, filename):
         print("Initializing grid....")
+        file = open(filename, "r") 
+        counter = 0
+        while True:
+            line = file.readline()
+            if not line:
+                break
+
+            # Grab the dimensions of the grid from the
+            # first line.
+            if counter == 0:
+                dimensions = line.split(' ')
+                assert len(dimensions) == 2
+                self.size = dimensions[0]
+                print("Grid size: ", str(self.size))
+            # Else populate the grid from the data directly.
+            else:
+                row = line.split(' ')
+            counter += 1
+        file.close()
