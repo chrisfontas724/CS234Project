7683b70317b1a3027597cae6171801e8e84541e1
Chris <chrisfontas724@gmail.com>
2020-02-18 21:28:33 -0800

    so few valid states

--- unittests.py
+++ unittests.py
@@ -5,42 +5,42 @@ import unittest
 
 class TestGridFunctions(unittest.TestCase):
 
-    # def test_basic_grid(self):
-    #     # Create grid.
-    #     grid = Grid(filename="levels/test_level.txt")
+    def test_basic_grid(self):
+        # Create grid.
+        grid = Grid(filename="levels/test_level.txt")
 
-    #     print("Number of valid states: ", len(grid.possible_states))
+        print("Number of valid states: ", len(grid.possible_states))
 
-    #     # Make sure the start flow locations are accurate.
-    #     self.assertEqual(grid.color_start_coords[1], (0,0))
-    #     self.assertEqual(grid.color_start_coords[2], (0,1))
-    #     self.assertEqual(grid.color_start_coords[3], (0,2))
+        # Make sure the start flow locations are accurate.
+        self.assertEqual(grid.color_start_coords[1], (0,0))
+        self.assertEqual(grid.color_start_coords[2], (0,1))
+        self.assertEqual(grid.color_start_coords[3], (0,2))
 
-    #     # Make sure the end flow locations are accurate.
-    #     self.assertEqual(grid.color_end_coords[1], (3,0))
-    #     self.assertEqual(grid.color_end_coords[2], (3,1))
-    #     self.assertEqual(grid.color_end_coords[3], (3,2))
+        # Make sure the end flow locations are accurate.
+        self.assertEqual(grid.color_end_coords[1], (3,0))
+        self.assertEqual(grid.color_end_coords[2], (3,1))
+        self.assertEqual(grid.color_end_coords[3], (3,2))
 
-    #     # There should only be 4 moves available at the beginning.
-    #     self.assertEqual(len(grid.possible_actions(grid.spaces)), 4)
+        # There should only be 4 moves available at the beginning.
+        self.assertEqual(len(grid.possible_actions(grid.spaces)), 4)
 
-    #     # The following bad moves should throw an exception due to
-    #     # the assert inside |possible_actions|.
-    #     with self.assertRaises(Exception) as context:
-    #         grid.next_state(grid.spaces, (1,0))
-    #     self.assertTrue('Action is not viable' in str(context.exception))
+        # The following bad moves should throw an exception due to
+        # the assert inside |possible_actions|.
+        with self.assertRaises(Exception) as context:
+            grid.next_state(grid.spaces, (1,0))
+        self.assertTrue('Action is not viable' in str(context.exception))
 
-    #     # Move red down, now there should be 5 moves available and the
-    #     # current tip for red should be at (1,0)
-    #     next_state = grid.next_state(grid.spaces, (1,2))
-    #     self.assertEqual(len(grid.possible_actions(next_state)), 5)
-    #     self.assertEqual(grid.color_flow_tips[1], (1,0))
+        # Move red down, now there should be 5 moves available and the
+        # current tip for red should be at (1,0)
+        next_state = grid.next_state(grid.spaces, (1,2))
+        self.assertEqual(len(grid.possible_actions(next_state)), 5)
+        self.assertEqual(grid.color_flow_tips[1], (1,0))
 
-    #     # Now try to move red back up, which it shouldn't be able to
-    #     # do because that's where it just came from.
-    #     with self.assertRaises(Exception) as context:
-    #         grid.next_state(next_state, (1,0))
-    #     self.assertTrue('Action is not viable' in str(context.exception))
+        # Now try to move red back up, which it shouldn't be able to
+        # do because that's where it just came from.
+        with self.assertRaises(Exception) as context:
+            grid.next_state(next_state, (1,0))
+        self.assertTrue('Action is not viable' in str(context.exception))
 
 
     # Test to make sure that the static Grid function |is_valid_state| works
@@ -87,7 +87,6 @@ class TestGridFunctions(unittest.TestCase):
         grid[1][1] = 2
         print_grid(True)
         self.assertFalse(Grid.is_valid_state(grid, 4, start_coords, end_coords))
-
 
         # Remove the above 2, turn the other 2 into a 3 and it should be invalid once more.
         grid[1][1] = 0
