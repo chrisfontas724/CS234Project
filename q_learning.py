3e24ec54da7373bada40884920a9bbbc0e3f6237
Chris Fontas <chrisfontas724@gmail.com>
2020-03-04 23:44:32 -0800

    more modifications

--- q_learning.py
+++ q_learning.py
@@ -8,11 +8,13 @@ import torch.nn as nn
 from renderer.renderer import GridRenderer
 
 
-def train(grid, mlp, loss_function, optimizer):
+def train(grid, mlp):
+	loss_function = nn.MSELoss()
+	optimizer = optim.SGD(mlp.parameters(), lr=0.001, momentum=0.9)
 
 	state = QState(grid.start_state)
 
-	train_steps = 1000
+	train_steps = 100000
 	for _ in range(train_steps):
 		features = state.get_feature_vector()
 
@@ -28,17 +30,17 @@ def train(grid, mlp, loss_function, optimizer):
 		new_state_q_values = mlp(new_features.float())
 		_, new_index = new_state_q_values.max(0)
 
-		print("Q Values: " + str(new_state_q_values[new_index]) + " " + str(old_state_q_values[index]))
+		#print("Q Values: " + str(new_state_q_values[new_index]) + " " + str(old_state_q_values[index]))
 
 		loss = loss_function(reward + new_state_q_values[new_index], old_state_q_values[index])
-		print("LOSS: ", loss)
+		print("LOSS: ", loss.item())
 		print("\n")
 
 		loss.backward()
 		optimizer.step()
 
-		for param in mlp.parameters():
-  			print(param.data)
+		# for param in mlp.parameters():
+  # 			print(param.data)
 
 	return mlp
 
@@ -97,9 +99,7 @@ def main():
 	mlp = MLP(mlp_config)
 	mlp = mlp.float()
 
-	criterion = nn.MSELoss()
-	optimizer = optim.SGD(mlp.parameters(), lr=0.001, momentum=0.9)
-	mlp = train(grid, mlp, criterion, optimizer)
+	mlp = train(grid, mlp)
 
 	status = play(grid, mlp)
 	print("We " + ("won \\^_^/" if status else "lost =("))
